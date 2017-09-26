6e827cd524ff71bd6604ff0216790d85b509a447
Ryan Pavlik <ryan@sensics.com>
2017-09-26 14:45:07 -0500

    Add an interface for bootloader entry.

---
+++ STBootLib/ISTBootEntry.cs
@@ -0,0 +1,11 @@
+﻿
+using System.IO.Ports;
+namespace STBootLib
+{
+    public interface ISTBootEntry
+    {
+        void AssertReset(SerialPort port, bool asserted);
+        void AssertBootloaderEntry(SerialPort port, bool asserted);
+        void ResetWait();
+    }
+}
